949ef1964137fc1535988993907e5046e7d553d8
Brandon Lew <25602300+blsnwbrdr@users.noreply.github.com>
2025-02-28 14:13:40 -0500

    move tip data fetch to listings body component and change to fetch once

--- src/screens/ListingsBody.js
+++ src/screens/ListingsBody.js
@@ -4,23 +4,36 @@ import CountryInfo from './CountryInfo';
 
 export default function ListingsBody() {
   const [display, setDisplay] = useState('CountryList');
-  const [countryTipData, setCountryTipData] = useState([]);
-  const [countryCurrencyData, setCountryCurrencyData] = useState('');
+  const [tipData, _setTipData] = useState([]);
+  const tipDataRef = useRef(tipData);
+  const setTipData = (newTipData) => {
+    tipDataRef.current = newTipData;
+    _setTipData(newTipData);
+  };
   const [currencyData, _setCurrencyData] = useState([]);
   const currencyDataRef = useRef(currencyData);
   const setCurrencyData = (newCurrencyData) => {
     currencyDataRef.current = newCurrencyData;
     _setCurrencyData(newCurrencyData);
   };
+  const [countryTipData, setCountryTipData] = useState([]);
+  const [countryCurrencyData, setCountryCurrencyData] = useState('');
 
-  // Get currency data
   useEffect(() => {
+    // Get country tip data
+    fetch('https://brandonscode.herokuapp.com/tipjar/tip-data')
+      .then((res) => res.json())
+      .then((result) => {
+        setTipData(result);
+      });
+
+    // Get currency data
     fetch('https://brandonscode.herokuapp.com/tipjar/currency-data')
       .then((res) => res.json())
       .then((result) => {
         setCurrencyData(result);
       });
-  });
+  }, []);
 
   // Display CountryList screen
   const displayCountryList = () => {
@@ -31,24 +44,32 @@ export default function ListingsBody() {
   const displayCountryInfo = (country) => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-    fetch(`https://brandonscode.herokuapp.com/tipjar/search/country/${country}`)
-      .then((res) => res.json())
-      .then((result) => {
-        for (var i = 0; i < currencyDataRef.current.length; i++) {
-          if (result.currency === currencyDataRef.current[i].currency) {
+    for (let x = 0; x < tipDataRef.current.length; x++) {
+      if (country === tipDataRef.current[x].country) {
+        for (let i = 0; i < currencyDataRef.current.length; i++) {
+          if (
+            tipDataRef.current[x].currency ===
+            currencyDataRef.current[i].currency
+          ) {
             setDisplay('CountryInfo');
-            setCountryTipData(result);
+            setCountryTipData(tipDataRef.current[x]);
             setCountryCurrencyData(
               Math.round(currencyDataRef.current[i].conversion * 100) / 100
             );
           }
         }
-      });
+      }
+    }
   };
 
   switch (display) {
     case 'CountryList':
-      return <CountryList displayCountryInfo={displayCountryInfo} />;
+      return (
+        <CountryList
+          displayCountryInfo={displayCountryInfo}
+          tipDataRef={tipDataRef}
+        />
+      );
     case 'CountryInfo':
       return (
         <CountryInfo
