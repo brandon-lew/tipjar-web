803b3d3db2a67428cc2fc37b735dc64b3431f78f
Brandon Lew <25602300+blsnwbrdr@users.noreply.github.com>
2025-02-28 12:03:48 -0500

    Revert "change to fetch data once"
    This reverts commit 128496a1c25c7bb05c3c340e04b8085272f61090.

--- src/screens/CountryList.js
+++ src/screens/CountryList.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 export default function CountryList(props) {
+  const [countryTipData, _setCountryTipData] = useState([]);
+  const countryTipDataRef = useRef(countryTipData);
+  const setCountryTipData = (newCountryTipData) => {
+    countryTipDataRef.current = newCountryTipData;
+    _setCountryTipData(newCountryTipData);
+  };
+
+  // Get country tip data
+  useEffect(() => {
+    fetch('https://brandonscode.herokuapp.com/tipjar/tip-data')
+      .then((res) => res.json())
+      .then((result) => {
+        setCountryTipData(result);
+      });
+  });
+
   return (
     <div className='body fadeIn'>
       <div className='list'>
         <ul>
-          {props.tipDataRef.current.map((data, key) => {
+          {countryTipDataRef.current.map((data, key) => {
             return (
               <li key={key}>
                 <button onClick={() => props.displayCountryInfo(data.country)}>
